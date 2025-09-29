347e5c67ea528451cd20f9410123622fdc9f42e3
tobam <tobamohaqqeq84@gmail.com>
2025-09-29 04:50:23 +0000

    uppdate

--- main.cpp
+++ main.cpp
@@ -25,5 +25,42 @@
        void setTitle(string movieTitle) {title = movieTitle;}
        string getTitle() {return title;}
 
+  void print(){
 
+     cout << "Title: " << title << endl;
+     cout << "Year published: " << yearReleased << endl;
+     cout << "Author: " << author << endl << endl;
+    
   }
+};
+
+int main() {
+ifstream inFile("input.txt");
+     if (!inFile) {
+         cerr << "Error: Unable to open the file"; << endl;
+             return 1; }
+     
+     }
+Movie movie[4];
+string writer, title;
+int year;
+
+
+for (int i=0; i<4; i++){
+     getline(inFile, title);
+     inFile >> year;
+     inFile.ignore();  // this ignores the new line.
+     getline(inFile, writer);
+
+
+     Movie temp;
+     temp.setTitle(title);
+     temp.setYear(year);
+     temp.setWriter(writer);
+
+     movie[i] = temp;
+}
+
+
+
+     
