e539481012c6f8b3406917db83aa83373de54686
tobam <tobamohaqqeq84@gmail.com>
2025-09-29 04:37:40 +0000

    2

--- main.cpp
+++ main.cpp
@@ -8,3 +8,22 @@
 #include<array>
 #include<fstream>
  using namespace std;
+
+  class Movie{
+    private:   
+         string title;
+         int yearReleased;
+         string title;
+    
+    public:
+       void setWriter(string writer) { screenWriter = writer;}
+       string getWriter() {return screenWriter;}
+
+       void setyear(int year) {yearReleased = year;}
+       int getYear() {return yearReleased;}
+
+       void setTitle(string movieTitle) {title = movieTitle;}
+       string getTitle() {return title;}
+
+
+  }
